e681135cb1dabc22c2522bf55b4438b045b7b63a
Alao Oluwaseyin <seyinalao@gmail.com>
2026-05-21 19:47:32 +0100

    Resolved Lint issues

--- src/app/(auth)/verification/page.tsx
+++ src/app/(auth)/verification/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
-import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Professional Verification',
