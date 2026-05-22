6c9fa63ea394053d632097d1eb2bdd9f6582afae
Ridwan <lawal6382@gmail.com>
2026-05-22 17:07:56 +0100

    feat: add the verification page navbar

--- src/app/(auth)/verification/layout.tsx
+++ src/app/(auth)/verification/layout.tsx
@@ -1,11 +1,12 @@
 import VerificationLayoutAnimation from '@/components/verification/VerificationLayoutAnimation';
+import VerificationNavbar from '@/components/verification/VerificationNavbar';
 
 import React, { PropsWithChildren } from 'react';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className="px-4 space-y-14 py-10">
-      {/* <VerificationNavbar /> */}
+      <VerificationNavbar />
 
       <VerificationLayoutAnimation>{children}</VerificationLayoutAnimation>
     </div>
