7a5726ca5dcd54bcabf6c5c7aa886bb1769e3cfd
ALADETAN-IFE <famousfotune123@gmail.com>
2026-05-29 17:09:39 +0100

    feat(overview): add CaseRequest and Cases components to OverviewPage

--- src/app/(doctors-dashboard)/user/page.tsx
+++ src/app/(doctors-dashboard)/user/page.tsx
@@ -1,4 +1,4 @@
-import { Summary } from '@/components/doctor/overview';
+import { Summary, CaseRequest, Cases } from '@/components/doctor/overview';
 import { getOverview, Overview } from '@/services/doctor';
 
 export default async function OverviewPage() {
@@ -9,6 +9,8 @@ export default async function OverviewPage() {
   return (
     <div className="flex flex-col gap-5 pt-2.5 pb-10 px-2.5">
       <Summary overview={overview as Overview} />
+      <CaseRequest overview={overview as Overview} />
+      <Cases overview={overview as Overview} />
     </div>
   );
 }
