1639cf754a4b136787fd74841796896120d52712
ALADETAN-IFE <famousfotune123@gmail.com>
2026-05-29 19:25:53 +0100

    fix(OverviewPage): comment out debug log for overview data

--- src/app/(doctors-dashboard)/user/page.tsx
+++ src/app/(doctors-dashboard)/user/page.tsx
@@ -4,7 +4,7 @@ import { getOverview, Overview } from '@/services/doctor';
 export default async function OverviewPage() {
   const overview = await getOverview();
 
-  console.log('overview (mock):', overview);
+  // console.log('overview (mock):', overview);
 
   return (
     <div className="flex flex-col gap-5 pt-2.5 pb-10 px-2.5">
